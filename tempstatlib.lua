5d90e59dd9f548ae8d58797d63131edb299863ec
Kerkel <134325525+drpandacat@users.noreply.github.com>
2025-04-05 11:37:03 -0400

    Update tempstatlib.lua

--- tempstatlib.lua
+++ tempstatlib.lua
@@ -1,3 +1,5 @@
+
+
 --[[
     Temporary Stat Library by Kerkel
     Version 1.0
@@ -8,43 +10,94 @@ return function (GetData)
     local VERSION = 1
 
     if TempStatLib then
-        if TempStatLib.Internal.VERSION > VERSION then
-            return
+        if TempStatLib.Internal.VERSION > VERSION then return end
+        for _, v in ipairs(TempStatLib.Internal.CallbackEntries) do
+            TempStatLib:RemoveCallback(v[1], v[3])
         end
-        TempStatLib.Internal:RemoveCallbacks()
     end
 
     TempStatLib = RegisterMod("Temporary Stat Library", 1)
     TempStatLib.Internal = {}
     TempStatLib.Internal.VERSION = VERSION
-    TempStatLib.Internal.CallbackEntries = {}
+    TempStatLib.Internal.Game = Game()
     TempStatLib.Internal.DEFAULT_FREQUENCY = 10
+    TempStatLib.Internal.CallbackEntries = {
+        {
+            ModCallbacks.MC_POST_PEFFECT_UPDATE,
+            CallbackPriority.DEFAULT,
+            ---@param player EntityPlayer
+            function (_, player)
+                local data = TempStatLib:GetData(player)
+                local eval, frame
 
-    local game = Game()
-    ---@param ID ModCallbacks
-    ---@param fn function
-    ---@param filter any
-    ---@param priority? CallbackPriority
-    function TempStatLib.Internal:AddCallback(ID, fn, filter, priority)
-        TempStatLib.Internal.CallbackEntries[#TempStatLib.Internal.CallbackEntries + 1] = {
-            ID = ID,
-            Fn = fn,
-            Filter = filter,
-            Priority = priority
+                for i = #data, 1, -1 do
+                    local v = data[i]
+
+                    if not v.Persistent and player.FrameCount == 0 then
+                        table.remove(data, i)
+                        player:AddCacheFlags(v.Stat)
+                        eval = true
+                    else
+                        frame = frame or TempStatLib.Internal.Game:GetFrameCount() + 1
+
+                        if (frame - v.ApplyFrame) % v.Frequency == 0 then
+                            local increase = v.Amount > 0
+
+                            v.Amount = v.Amount - v.ChangeAmount
+
+                            if (increase and v.Amount <= 0) or (not increase and v.Amount >= 0) then
+                                table.remove(data, i)
+                            end
+
+                            player:AddCacheFlags(v.Stat)
+
+                            eval = true
+                        end
+                    end
+                end
+
+                if eval then
+                    player:EvaluateItems()
+                end
+            end
+        },
+        {
+            ModCallbacks.MC_EVALUATE_CACHE,
+            CallbackPriority.DEFAULT,
+            ---@param player EntityPlayer
+            ---@param flag CacheFlag
+            function (_, player, flag)
+                local data = TempStatLib:GetData(player)
+
+                if #data > 0 then
+                    for _, v in ipairs(data) do
+                        if v.Stat == CacheFlag.CACHE_DAMAGE and flag == CacheFlag.CACHE_DAMAGE then
+                            player.Damage = player.Damage + v.Amount
+                        elseif v.Stat == CacheFlag.CACHE_FIREDELAY and flag == CacheFlag.CACHE_FIREDELAY then
+                            player.MaxFireDelay = 30 / (30 / (player.MaxFireDelay + 1) + v.Amount) - 1
+                        elseif v.Stat == CacheFlag.CACHE_SHOTSPEED and flag == CacheFlag.CACHE_SHOTSPEED then
+                            player.ShotSpeed = player.ShotSpeed + v.Amount
+                        elseif v.Stat == CacheFlag.CACHE_RANGE and flag == CacheFlag.CACHE_RANGE then
+                            player.TearRange = player.TearRange + v.Amount * 40
+                        elseif v.Stat == CacheFlag.CACHE_SPEED and flag == CacheFlag.CACHE_SPEED then
+                            player.MoveSpeed = player.MoveSpeed + v.Amount
+                        elseif v.Stat == CacheFlag.CACHE_LUCK and flag == CacheFlag.CACHE_LUCK then
+                            player.Luck = player.Luck + v.Amount
+                        end
+                    end
+                end
+            end
         }
-    end
+    }
 
-    function TempStatLib.Internal:RemoveCallbacks()
-        for _, v in ipairs(TempStatLib.Internal.CallbackEntries) do
-            TempStatLib:RemoveCallback(v.ID, v.Fn)
-        end
+    for _, v in ipairs(TempStatLib.Internal.CallbackEntries) do
+        TempStatLib:AddPriorityCallback(v[1], v[2], v[3], v[4])
     end
 
     ---@param entity Entity
     function TempStatLib:GetData(entity)
         return GetData(entity)
     end
-
 
     ---@class TempStatConfig
     ---@field Stat CacheFlag
@@ -71,7 +124,7 @@ return function (GetData)
         for i, v in ipairs(data) do
             if v.Identifier == config.Identifier then
                 data[i].Amount = config.Amount + v.Amount
-                data[i].ApplyFrame = game:GetFrameCount()
+                data[i].ApplyFrame = TempStatLib.Internal.Game:GetFrameCount()
                 player:AddCacheFlags(config.Stat)
                 player:EvaluateItems()
                 return data[i]
@@ -87,7 +140,7 @@ return function (GetData)
             Amount = config.Amount,
             ChangeAmount = config.Amount / config.Duration * config.Frequency,
             Stat = config.Stat,
-            ApplyFrame = game:GetFrameCount(),
+            ApplyFrame = TempStatLib.Internal.Game:GetFrameCount(),
             Identifier = config.Identifier,
         }
 
@@ -109,74 +162,6 @@ return function (GetData)
             if v.Identifier == identifier then
                 return data[i]
             end
-        end
-    end
-
-    ---@param player EntityPlayer
-    TempStatLib.Internal:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, function (_, player)
-        local data = TempStatLib:GetData(player)
-        local eval, frame
-
-        for i = #data, 1, -1 do
-            local v = data[i]
-
-            if not v.Persistent and player.FrameCount == 0 then
-                table.remove(data, i)
-                player:AddCacheFlags(v.Stat)
-                eval = true
-            else
-                frame = frame or game:GetFrameCount() + 1
-
-                if (frame - v.ApplyFrame) % v.Frequency == 0 then
-                    local increase = v.Amount > 0
-
-                    v.Amount = v.Amount - v.ChangeAmount
-
-                    if (increase and v.Amount <= 0) or (not increase and v.Amount >= 0) then
-                        table.remove(data, i)
-                    end
-
-                    player:AddCacheFlags(v.Stat)
-
-                    eval = true
-                end
-            end
-        end
-
-        if eval then
-            player:EvaluateItems()
-        end
-    end)
-
-    ---@param player EntityPlayer
-    ---@param flag CacheFlag
-    TempStatLib.Internal:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, function (_, player, flag)
-        local data = TempStatLib:GetData(player)
-
-        if #data > 0 then
-            for _, v in ipairs(data) do
-                if v.Stat == CacheFlag.CACHE_DAMAGE and flag == CacheFlag.CACHE_DAMAGE then
-                    player.Damage = player.Damage + v.Amount
-                elseif v.Stat == CacheFlag.CACHE_FIREDELAY and flag == CacheFlag.CACHE_FIREDELAY then
-                    player.MaxFireDelay = 30 / (30 / (player.MaxFireDelay + 1) + v.Amount) - 1
-                elseif v.Stat == CacheFlag.CACHE_SHOTSPEED and flag == CacheFlag.CACHE_SHOTSPEED then
-                    player.ShotSpeed = player.ShotSpeed + v.Amount
-                elseif v.Stat == CacheFlag.CACHE_RANGE and flag == CacheFlag.CACHE_RANGE then
-                    player.TearRange = player.TearRange + v.Amount * 40
-                elseif v.Stat == CacheFlag.CACHE_SPEED and flag == CacheFlag.CACHE_SPEED then
-                    player.MoveSpeed = player.MoveSpeed + v.Amount
-                elseif v.Stat == CacheFlag.CACHE_LUCK and flag == CacheFlag.CACHE_LUCK then
-                    player.Luck = player.Luck + v.Amount
-                end
-            end
-        end
-    end)
-
-    for _, v in ipairs(TempStatLib.Internal.CallbackEntries) do
-        if v.Priority then
-            TempStatLib:AddPriorityCallback(v.ID, v.Priority, v.Fn, v.Filter)
-        else
-            TempStatLib:AddCallback(v.ID, v.Fn, v.FIlter)
         end
     end
 end
